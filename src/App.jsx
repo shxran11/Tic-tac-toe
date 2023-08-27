a5e60f4aac892239d6c3ac7fdce1ca87954d3fbb
Sharanya <saranya.podder.fiem.cse21@teamfuture.in>
2023-08-27 12:28:55 +0530

    Added title and fonts

--- src/App.jsx
+++ src/App.jsx
@@ -61,6 +61,11 @@ function App() {
 
   return (
     <div className="app">
+      <h1>
+        <span className="text-blue">TIC</span>-
+        <span className="text-yellow">TAC</span>-
+        <span className="text-blue">TOE</span>
+      </h1>
       <StatusMessage winner={winner} gamingBoard={gamingBoard} />
       <Board
         squares={gamingBoard.squares}
@@ -75,7 +80,13 @@ function App() {
       >
         Start new game
       </button>
-      <h2>Current game history</h2>
+      <h2
+        style={{
+          fontWeight: 'normal',
+        }}
+      >
+        Current game history
+      </h2>
       <History history={history} moveTo={moveTo} currentMove={currentMove} />
     </div>
   );
