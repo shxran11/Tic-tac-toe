5fe8f0a37bbb4d5ba1238d86a493117b129a69df
Sharanya <saranya.podder.fiem.cse21@teamfuture.in>
2023-08-27 12:59:01 +0530

    Edited background

--- src/App.jsx
+++ src/App.jsx
@@ -88,6 +88,7 @@ function App() {
         Current game history
       </h2>
       <History history={history} moveTo={moveTo} currentMove={currentMove} />
+      <div className="bg-balls" />
     </div>
   );
 }
